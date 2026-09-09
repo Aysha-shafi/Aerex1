417b27ad215a7fab27e0fc1b4dac6357574e337b
Aysha Shafi <ayshaashafi003@gmail.com>
2026-09-09 17:01:36 +0530

    latest new

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/log.jpg";
+import logo from "../assets/lo.jpeg";
 export default function Logo() {
   return <img src={logo} alt="AEREX" style={{ height: 50, width: "auto", display: "block" }} />;
 }
